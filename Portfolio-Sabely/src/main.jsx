bda9b5e08996c24153362befecb85b068bc68790
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-25 15:49:08 -0400

    added the main jsx back it

--- Portfolio-Sabely/src/main.jsx
+++ Portfolio-Sabely/src/main.jsx
@@ -1,10 +1,53 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App';
+import Home from './componts/pages/Home';
+import Contact from './components/pages/Contact';
+import About from './components/pages/AboutMe';
+import Resume from './components/pages/Resume';
+import Projects from './components/pages/Projects';
+import ErrorPage from './components/pages/ErrorPage';
+
+const router = createBrowerRouter([
+  {
+    path:'/',
+    element:<App />,
+    errorElement: <ErrorPage />,
+    children: [{
+      index: true,
+      element: <Home />
+    },
+    {
+      path: 'home',
+      element: <Home />,
+    },
+    {
+      path: 'about',
+      element: <About />,
+    },
+    {
+      path: 'resume',
+      element: <Resume />,
+    },
+    {
+      path: 'contact',
+      element: <Contact />,
+    },
+    {
+      path: 'projects',
+      element: <Projects />,
+    },
+    ],
+  },
+  {
+    path: 'error-page',
+    element:<ErrorPage />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
