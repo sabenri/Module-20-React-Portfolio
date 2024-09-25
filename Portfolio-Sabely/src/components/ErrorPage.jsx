a52eabceb0ff2fa8fdd9cf29d55829ed1185d51e
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-25 18:58:40 -0400

    added the error page jsx

---
+++ Portfolio-Sabely/src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/error.css'
+
+function ErrorPage() {
+    return (
+        <div className="error-page-container">
+            <h1>Sorry This Page Is Not Avilable</h1>
+            <p>Sorry, The Page You're Trying To Vist Is Not Avilable</p>
+        </div>
+    );
+}
+
+export default ErrorPage;
