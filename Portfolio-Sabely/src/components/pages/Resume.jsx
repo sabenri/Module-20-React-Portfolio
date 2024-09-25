9032f13cf9daa4165f86ebbc9012a29efd2f8ef8
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-25 18:32:07 -0400

    added resume jsx

---
+++ Portfolio-Sabely/src/components/pages/Resume.jsx
@@ -0,0 +1,15 @@
+function Resume () {
+    return (
+        <div className="resume-container text-center">
+            <h1>Visit My GitHub For More Projects And Details</h1>
+            <a 
+            href="https://github.com/sabenri"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+            >
+                <button className="btn btn-primary">Vist My Github</button>
+            </a>
+        </div>
+    );
+}
