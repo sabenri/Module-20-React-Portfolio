5799bdb3d8d35b217f84ffb47f52541c5e09a000
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-25 16:13:06 -0400

    started making the pages made home jsx and about me jsx

---
+++ Portfolio-Sabely/src/components/pages/AboutMe.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import image from '../../assets/'
+import'../../styles/style.css';
+
+function AboutMe() {
+    return (
+        <div className="about-me-div">
+            <div className="about-me-img">
+                <img src="" alt="" />
+            </div>
+            <div className="about-me-text">
+                <h3>Info About Me:</h3>
+                <p>I am lreaning to be a full stack developer. I enjoy working with HTML, CSS, JavaSript and React!</p>
+                <p>I am a cat lover I have 3 cats, I enjoy many differnt forms of art. I love to draw crochet and craft.</p>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutMe 
---
+++ Portfolio-Sabely/src/components/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1> Welcome To My Portfolio!!</h1>
+            <p>I'm Happy To Have You Here</p>
+        </div>
+    );
+}
+
+export default Home;
